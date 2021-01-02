cbebbe47dc5d41982ae269289c8394d62c359051
John Rios <jlrjr98@gmail.com>
2021-01-01 14:59:19 -1000

    Add Question model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { TriviaComponent } from './components/trivia/trivia.component';
 
 const routes: Routes = [
 
@@ -19,11 +18,6 @@ const routes: Routes = [
   {
     path : 'register',
     component : RegisterComponent
-  },
-
-  {
-    path: 'trivia',
-    component: TriviaComponent
   }
 ];
 
