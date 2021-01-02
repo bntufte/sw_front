85d78395d570d46d588faae7f1236396e5182374
John Rios <jlrjr98@gmail.com>
2021-01-01 15:18:58 -1000

    Mirror backend models in frontend

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,2 +1,4 @@
 export class Question {
+
+    constructor(public questionID: number, public question: string, public answer: string) { }
 }
---
+++ src/app/models/showcase.spec.ts
@@ -0,0 +1,7 @@
+import { Showcase } from './showcase';
+
+describe('Showcase', () => {
+  it('should create an instance', () => {
+    expect(new Showcase()).toBeTruthy();
+  });
+});
