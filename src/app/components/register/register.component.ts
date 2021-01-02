e3e801eddae403419697bebab1b7eefcc5788f82
logriffith <logriffith1991@gmail.com>
2021-01-01 20:28:10 -0700

    login and register done except for logic

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavBarService } from 'src/app/services/nav-bar.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public nav : NavBarService) { }
 
   ngOnInit(): void {
+    this.nav.hide();
   }
 
 }
