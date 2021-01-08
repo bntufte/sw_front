17a2eb7569daf4f3199e5eea5fea870120d21cfa
jlrjr98 <73042836+jlrjr98@users.noreply.github.com>
2021-01-08 01:49:15 -1000

    Revert "Scoreboard"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 import { RewardComponent } from './components/reward/reward.component';
 import { TriviaComponent } from './components/trivia/trivia.component';
-import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 
 const routes: Routes = [
 
@@ -35,12 +33,7 @@ const routes: Routes = [
     path: 'trivia',
     component : TriviaComponent
   },
-  
-  {
-    path: 'leaderboard',
-    component : ScoreboardComponent
-  },
-  
+
   {
     path: 'profile',
     component : ProfileComponent
@@ -50,6 +43,7 @@ const routes: Routes = [
     path: 'reward',
     component : RewardComponent
   }
+  
 ];
 
 @NgModule({
