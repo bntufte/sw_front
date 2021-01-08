7432ef6e085ebecfadb44b3d1f53394af30bdb04
logriffith <logriffith1991@gmail.com>
2021-01-07 19:20:15 -0700

    working on the updating a user

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NewUser } from 'src/app/models/new-user';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +14,20 @@ export class ProfileComponent implements OnInit {
   oldPassword : string;
   newPassword : string;
   confirmPassword : string;
+  message : string = "";
 
-  constructor(private profile : ProfileComponent) { }
+  constructor(private profile : ProfileService, public login : LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  updateUser(){
+    let updateUser = new NewUser(this.login.username, this.newPassword, this.confirmPassword);
+    console.log(updateUser);
+    if (updateUser.password === updateUser.confirmPassword){
+      let user = new User(this.login.username, this.newPassword);
+      this.profile.updateProfile(updateUser).subscribe();
+    }
   }
 
 }
